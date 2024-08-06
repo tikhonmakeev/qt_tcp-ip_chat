d16607de4834d6ddaa0ee29aef951e5c9f473e9f
tikhonmakeev <tihon.makeev@yandex.ru>
2024-08-06 21:01:32 +0300

    update build and make files

--- Client/moc_mainwindow.cpp
+++ Client/moc_mainwindow.cpp
@@ -1,106 +1,62 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created by: The Qt Meta Object Compiler version 68 (Qt 6.6.1)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.3)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "mainwindow.h"
-#include <QtCore/qmetatype.h>
-
-#if __has_include(<QtCore/qtmochelpers.h>)
-#include <QtCore/qtmochelpers.h>
-#else
-QT_BEGIN_MOC_NAMESPACE
-#endif
-
-
 #include <memory>
-
+#include "mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'mainwindow.h' doesn't include <QObject>."
-#elif Q_MOC_OUTPUT_REVISION != 68
-#error "This file was generated using the moc from 6.6.1. It"
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.3. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
 
-#ifndef Q_CONSTINIT
-#define Q_CONSTINIT
-#endif
-
+QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
-QT_WARNING_DISABLE_GCC("-Wuseless-cast")
-namespace {
-
-#ifdef QT_MOC_HAS_STRINGDATA
-struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {};
-static constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers::stringData(
-    "MainWindow",
-    "ReadytoRead",
-    "",
-    "on_pushButton_clicked",
-    "on_sendBtn_clicked",
-    "keyPressEvent",
-    "QKeyEvent*",
-    "e",
-    "on_username_editingFinished",
-    "on_hostAddress_editingFinished",
-    "on_userMsg_editingFinished"
-);
-#else  // !QT_MOC_HAS_STRING_DATA
-struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
-    uint offsetsAndSizes[22];
-    char stringdata0[11];
-    char stringdata1[12];
-    char stringdata2[1];
-    char stringdata3[22];
-    char stringdata4[19];
-    char stringdata5[14];
-    char stringdata6[11];
-    char stringdata7[2];
-    char stringdata8[28];
-    char stringdata9[31];
-    char stringdata10[27];
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[11];
+    char stringdata0[178];
 };
-#define QT_MOC_LITERAL(ofs, len) \
-    uint(sizeof(qt_meta_stringdata_CLASSMainWindowENDCLASS_t::offsetsAndSizes) + ofs), len 
-Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_stringdata_CLASSMainWindowENDCLASS = {
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
     {
-        QT_MOC_LITERAL(0, 10),  // "MainWindow"
-        QT_MOC_LITERAL(11, 11),  // "ReadytoRead"
-        QT_MOC_LITERAL(23, 0),  // ""
-        QT_MOC_LITERAL(24, 21),  // "on_pushButton_clicked"
-        QT_MOC_LITERAL(46, 18),  // "on_sendBtn_clicked"
-        QT_MOC_LITERAL(65, 13),  // "keyPressEvent"
-        QT_MOC_LITERAL(79, 10),  // "QKeyEvent*"
-        QT_MOC_LITERAL(90, 1),  // "e"
-        QT_MOC_LITERAL(92, 27),  // "on_username_editingFinished"
-        QT_MOC_LITERAL(120, 30),  // "on_hostAddress_editingFinished"
-        QT_MOC_LITERAL(151, 26)   // "on_userMsg_editingFinished"
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 11), // "ReadytoRead"
+QT_MOC_LITERAL(2, 23, 0), // ""
+QT_MOC_LITERAL(3, 24, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(4, 46, 18), // "on_sendBtn_clicked"
+QT_MOC_LITERAL(5, 65, 13), // "keyPressEvent"
+QT_MOC_LITERAL(6, 79, 10), // "QKeyEvent*"
+QT_MOC_LITERAL(7, 90, 1), // "e"
+QT_MOC_LITERAL(8, 92, 27), // "on_username_editingFinished"
+QT_MOC_LITERAL(9, 120, 30), // "on_hostAddress_editingFinished"
+QT_MOC_LITERAL(10, 151, 26) // "on_userMsg_editingFinished"
+
     },
-    "MainWindow",
-    "ReadytoRead",
-    "",
-    "on_pushButton_clicked",
-    "on_sendBtn_clicked",
-    "keyPressEvent",
-    "QKeyEvent*",
-    "e",
-    "on_username_editingFinished",
-    "on_hostAddress_editingFinished",
+    "MainWindow\0ReadytoRead\0\0on_pushButton_clicked\0"
+    "on_sendBtn_clicked\0keyPressEvent\0"
+    "QKeyEvent*\0e\0on_username_editingFinished\0"
+    "on_hostAddress_editingFinished\0"
     "on_userMsg_editingFinished"
 };
 #undef QT_MOC_LITERAL
-#endif // !QT_MOC_HAS_STRING_DATA
-} // unnamed namespace
 
-Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
+static const uint qt_meta_data_MainWindow[] = {
 
  // content:
-      12,       // revision
+       8,       // revision
        0,       // classname
        0,    0, // classinfo
        7,   14, // methods
@@ -110,14 +66,14 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
        0,       // flags
        0,       // signalCount
 
- // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   56,    2, 0x0a,    1 /* Public */,
-       3,    0,   57,    2, 0x08,    2 /* Private */,
-       4,    0,   58,    2, 0x08,    3 /* Private */,
-       5,    1,   59,    2, 0x08,    4 /* Private */,
-       8,    0,   62,    2, 0x08,    6 /* Private */,
-       9,    0,   63,    2, 0x08,    7 /* Private */,
-      10,    0,   64,    2, 0x08,    8 /* Private */,
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   49,    2, 0x0a /* Public */,
+       3,    0,   50,    2, 0x08 /* Private */,
+       4,    0,   51,    2, 0x08 /* Private */,
+       5,    1,   52,    2, 0x08 /* Private */,
+       8,    0,   55,    2, 0x08 /* Private */,
+       9,    0,   56,    2, 0x08 /* Private */,
+      10,    0,   57,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -131,34 +87,6 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
        0        // eod
 };
 
-Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
-    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
-    qt_meta_stringdata_CLASSMainWindowENDCLASS.offsetsAndSizes,
-    qt_meta_data_CLASSMainWindowENDCLASS,
-    qt_static_metacall,
-    nullptr,
-    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSMainWindowENDCLASS_t,
-        // Q_OBJECT / Q_GADGET
-        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
-        // method 'ReadytoRead'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'on_pushButton_clicked'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'on_sendBtn_clicked'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'keyPressEvent'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        QtPrivate::TypeAndForceComplete<QKeyEvent *, std::false_type>,
-        // method 'on_username_editingFinished'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'on_hostAddress_editingFinished'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'on_userMsg_editingFinished'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>
-    >,
-    nullptr
-} };
-
 void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     if (_c == QMetaObject::InvokeMetaMethod) {
@@ -168,7 +96,7 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 0: _t->ReadytoRead(); break;
         case 1: _t->on_pushButton_clicked(); break;
         case 2: _t->on_sendBtn_clicked(); break;
-        case 3: _t->keyPressEvent((*reinterpret_cast< std::add_pointer_t<QKeyEvent*>>(_a[1]))); break;
+        case 3: _t->keyPressEvent((*reinterpret_cast< QKeyEvent*(*)>(_a[1]))); break;
         case 4: _t->on_username_editingFinished(); break;
         case 5: _t->on_hostAddress_editingFinished(); break;
         case 6: _t->on_userMsg_editingFinished(); break;
@@ -176,6 +104,16 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         }
     }
 }
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
 
 const QMetaObject *MainWindow::metaObject() const
 {
@@ -185,7 +123,7 @@ const QMetaObject *MainWindow::metaObject() const
 void *MainWindow::qt_metacast(const char *_clname)
 {
     if (!_clname) return nullptr;
-    if (!strcmp(_clname, qt_meta_stringdata_CLASSMainWindowENDCLASS.stringdata0))
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
         return static_cast<void*>(this);
     return QMainWindow::qt_metacast(_clname);
 }
@@ -201,9 +139,10 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
         _id -= 7;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
         if (_id < 7)
-            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+            *reinterpret_cast<int*>(_a[0]) = -1;
         _id -= 7;
     }
     return _id;
 }
 QT_WARNING_POP
+QT_END_MOC_NAMESPACE
--- Client/moc_notificationclient.cpp
+++ Client/moc_notificationclient.cpp
@@ -1,78 +1,52 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'notificationclient.h'
 **
-** Created by: The Qt Meta Object Compiler version 68 (Qt 6.6.1)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.3)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "notificationclient.h"
-#include <QtCore/qmetatype.h>
-
-#if __has_include(<QtCore/qtmochelpers.h>)
-#include <QtCore/qtmochelpers.h>
-#else
-QT_BEGIN_MOC_NAMESPACE
-#endif
-
-
 #include <memory>
-
+#include "notificationclient.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'notificationclient.h' doesn't include <QObject>."
-#elif Q_MOC_OUTPUT_REVISION != 68
-#error "This file was generated using the moc from 6.6.1. It"
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.3. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
 
-#ifndef Q_CONSTINIT
-#define Q_CONSTINIT
-#endif
-
+QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
-QT_WARNING_DISABLE_GCC("-Wuseless-cast")
-namespace {
-
-#ifdef QT_MOC_HAS_STRINGDATA
-struct qt_meta_stringdata_CLASSNotificationClientENDCLASS_t {};
-static constexpr auto qt_meta_stringdata_CLASSNotificationClientENDCLASS = QtMocHelpers::stringData(
-    "NotificationClient",
-    "notificationChanged",
-    "",
-    "updateNotification"
-);
-#else  // !QT_MOC_HAS_STRING_DATA
-struct qt_meta_stringdata_CLASSNotificationClientENDCLASS_t {
-    uint offsetsAndSizes[8];
-    char stringdata0[19];
-    char stringdata1[20];
-    char stringdata2[1];
-    char stringdata3[19];
+struct qt_meta_stringdata_NotificationClient_t {
+    QByteArrayData data[4];
+    char stringdata0[59];
 };
-#define QT_MOC_LITERAL(ofs, len) \
-    uint(sizeof(qt_meta_stringdata_CLASSNotificationClientENDCLASS_t::offsetsAndSizes) + ofs), len 
-Q_CONSTINIT static const qt_meta_stringdata_CLASSNotificationClientENDCLASS_t qt_meta_stringdata_CLASSNotificationClientENDCLASS = {
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_NotificationClient_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_NotificationClient_t qt_meta_stringdata_NotificationClient = {
     {
-        QT_MOC_LITERAL(0, 18),  // "NotificationClient"
-        QT_MOC_LITERAL(19, 19),  // "notificationChanged"
-        QT_MOC_LITERAL(39, 0),  // ""
-        QT_MOC_LITERAL(40, 18)   // "updateNotification"
+QT_MOC_LITERAL(0, 0, 18), // "NotificationClient"
+QT_MOC_LITERAL(1, 19, 19), // "notificationChanged"
+QT_MOC_LITERAL(2, 39, 0), // ""
+QT_MOC_LITERAL(3, 40, 18) // "updateNotification"
+
     },
-    "NotificationClient",
-    "notificationChanged",
-    "",
-    "updateNotification"
+    "NotificationClient\0notificationChanged\0"
+    "\0updateNotification"
 };
 #undef QT_MOC_LITERAL
-#endif // !QT_MOC_HAS_STRING_DATA
-} // unnamed namespace
 
-Q_CONSTINIT static const uint qt_meta_data_CLASSNotificationClientENDCLASS[] = {
+static const uint qt_meta_data_NotificationClient[] = {
 
  // content:
-      12,       // revision
+       8,       // revision
        0,       // classname
        0,    0, // classinfo
        2,   14, // methods
@@ -82,11 +56,11 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSNotificationClientENDCLASS[] = {
        0,       // flags
        1,       // signalCount
 
- // signals: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   26,    2, 0x06,    1 /* Public */,
+ // signals: name, argc, parameters, tag, flags
+       1,    0,   24,    2, 0x06 /* Public */,
 
- // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       3,    0,   27,    2, 0x08,    2 /* Private */,
+ // slots: name, argc, parameters, tag, flags
+       3,    0,   25,    2, 0x08 /* Private */,
 
  // signals: parameters
     QMetaType::Void,
@@ -96,23 +70,6 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSNotificationClientENDCLASS[] = {
 
        0        // eod
 };
-
-Q_CONSTINIT const QMetaObject NotificationClient::staticMetaObject = { {
-    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
-    qt_meta_stringdata_CLASSNotificationClientENDCLASS.offsetsAndSizes,
-    qt_meta_data_CLASSNotificationClientENDCLASS,
-    qt_static_metacall,
-    nullptr,
-    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSNotificationClientENDCLASS_t,
-        // Q_OBJECT / Q_GADGET
-        QtPrivate::TypeAndForceComplete<NotificationClient, std::true_type>,
-        // method 'notificationChanged'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'updateNotification'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>
-    >,
-    nullptr
-} };
 
 void NotificationClient::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
@@ -128,7 +85,7 @@ void NotificationClient::qt_static_metacall(QObject *_o, QMetaObject::Call _c, i
         int *result = reinterpret_cast<int *>(_a[0]);
         {
             using _t = void (NotificationClient::*)();
-            if (_t _q_method = &NotificationClient::notificationChanged; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&NotificationClient::notificationChanged)) {
                 *result = 0;
                 return;
             }
@@ -136,6 +93,16 @@ void NotificationClient::qt_static_metacall(QObject *_o, QMetaObject::Call _c, i
     }
     (void)_a;
 }
+
+QT_INIT_METAOBJECT const QMetaObject NotificationClient::staticMetaObject = { {
+    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
+    qt_meta_stringdata_NotificationClient.data,
+    qt_meta_data_NotificationClient,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
 
 const QMetaObject *NotificationClient::metaObject() const
 {
@@ -145,7 +112,7 @@ const QMetaObject *NotificationClient::metaObject() const
 void *NotificationClient::qt_metacast(const char *_clname)
 {
     if (!_clname) return nullptr;
-    if (!strcmp(_clname, qt_meta_stringdata_CLASSNotificationClientENDCLASS.stringdata0))
+    if (!strcmp(_clname, qt_meta_stringdata_NotificationClient.stringdata0))
         return static_cast<void*>(this);
     return QObject::qt_metacast(_clname);
 }
@@ -161,7 +128,7 @@ int NotificationClient::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
         _id -= 2;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
         if (_id < 2)
-            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+            *reinterpret_cast<int*>(_a[0]) = -1;
         _id -= 2;
     }
     return _id;
@@ -173,3 +140,4 @@ void NotificationClient::notificationChanged()
     QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
 }
 QT_WARNING_POP
+QT_END_MOC_NAMESPACE
